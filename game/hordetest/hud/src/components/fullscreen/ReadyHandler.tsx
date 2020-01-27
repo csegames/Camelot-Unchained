44e60f03141d5f0b7907e0e90ee0c5089b40abe3
Ajackster <jacksonal300@gmail.com>
2020-01-27 15:29:12 -0500

    more

--- game/hordetest/hud/src/components/fullscreen/ReadyHandler.tsx
+++ game/hordetest/hud/src/components/fullscreen/ReadyHandler.tsx
@@ -19,15 +19,38 @@ const imagePreloader = [
   'images/fullscreen/fullscreen-career-bg.jpg',
   'images/fullscreen/fullscreen-image-bg.jpg',
   'images/fullscreen/fullscreen-scene-bg.jpg',
+
+  'images/fullscreen/startscreen/store/bundle-champ.jpg',
+  'images/fullscreen/startscreen/store/bundle-weapon.jpg',
+  'images/fullscreen/startscreen/store/skins-amazon.png',
+  'images/fullscreen/startscreen/store/skins-berserker.png',
+  'images/fullscreen/startscreen/store/skins-celt.png',
+  'images/fullscreen/startscreen/store/skins-knight.png',
+  'images/fullscreen/startscreen/store/weapons-axe.jpg',
+  'images/fullscreen/startscreen/store/weapons-bg-blue.jpg',
+  'images/fullscreen/startscreen/store/weapons-bg-green.jpg',
+  'images/fullscreen/startscreen/store/weapons-bg-red.jpg',
+  'images/fullscreen/startscreen/store/weapons-bg-yellow.jpg',
+  'images/fullscreen/startscreen/store/weapons-bg.png',
+  'images/fullscreen/startscreen/store/weapon-bow.png',
+  'images/fullscreen/startscreen/store/weapon-spear.png',
+  'images/fullscreen/startscreen/store/weapons-sword.png',
 ];
 
 export interface Props {
 }
 
-export function ReadyHandler(props: Props) {
-  return (
-    <Container>
+export interface State {
+}
 
-    </Container>
-  );
+export class ReadyHandler extends React.Component<Props, State> {
+  public render() {
+    return (
+      <Container>
+        {imagePreloader.map(() => {
+
+        })}
+      </Container>
+    );
+  }
 }
